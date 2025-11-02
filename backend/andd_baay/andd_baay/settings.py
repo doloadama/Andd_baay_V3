c340b8e707d50afc00c9d5b53860da02b703426a
adamadolo <doloadama@outlook.com>
2025-11-02 18:27:01 +0000

    feat: Update CORS settings and remove unused modal components

--- backend/andd_baay/andd_baay/settings.py
+++ backend/andd_baay/andd_baay/settings.py
@@ -110,5 +110,5 @@ SIMPLE_JWT = {
 # --- CORS Settings ---
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", # Add your frontend URL here
-    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
 ]
